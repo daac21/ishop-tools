958400172dee1926345cce623a84b78b1b6dc538
daac21 <dack2106@gmail.com>
2026-09-13 02:21:06 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ishop-tools-v3";
+const CACHE_NAME = "ishop-tools-v4";
 const ASSETS = [
   "./index.html",
   "./style.css",
@@ -7,7 +7,8 @@ const ASSETS = [
   "./datos/tradein.json",
   "./datos/precios_iphone.json",
   "./datos/applecare.json",
-  "./datos/switchup_modelos.json"
+  "./datos/switchup_modelos.json",
+  "./datos/financiamiento.json"
 ];
 
 self.addEventListener("install", (event) => {
